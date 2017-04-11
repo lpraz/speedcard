64fa3f90baca687dcb9c3f7f9f6431425bf6e3c1
lpraz <lpraz@mts.net>
2017-04-10 22:00:28 -0500

    Started implementing answering cards

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
+String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+};
+
+var cards = [];
+var activeCard;
+
 function onLoad() {
     // TODO: implement images
-    var cards = [];
     
+    // Push cards onto array
     cards.push({
         answer: 'Rock',
         text: 'Scissors'
@@ -17,26 +27,43 @@ function onLoad() {
         text: 'Paper'
     });
     
+    // Populate row of buttons based on array
     addButtons(cards);
+    
+    flash();
 }
 
 function addButtons(cards) {
-    var buttons = document.getElementById('buttons');
-    var template = 
-        '<li class="button" onClick="onClickButton()">\n' +
+    var TEMPLATE = 
+        '<li class="button" onClick="onClickButton({0})">\n' +
         '   <p class="button-number">\n'+
-        '       0\n' +
+        '       {1}\n' +
         '   </p>\n' +
         '   <p>\n' +
-        '       Placeholder\n' +
+        '       {2}\n' +
         '   </p>\n' +
         '</li>';
+    var buttons = document.getElementById('buttons');
     
     for (var i = 0; i < cards.length; i++) {
-        buttons.innerHTML += template;
+        buttons.innerHTML += TEMPLATE.format(i, i + 1, cards[i].answer);
     }
 }
 
+function flash() {
+    var text = document.getElementById('flashcard-text');
+    
+    activeCard = Math.floor(Math.random() * cards.length);
+    text.innerHTML = cards[activeCard].text;
+    
+    setTimeout(flash, 3000);
+}
+
 function onClickButton(id) {
-    document.getElementById('flashcard-text').innerHTML = "Clicked " + id;
+    var message = document.getElementById('message');
+    
+    if (id == activeCard)
+        message.innerHTML = 'Correct!';
+    else
+        message.innerHTML = 'Wrong, try again!';
 }
